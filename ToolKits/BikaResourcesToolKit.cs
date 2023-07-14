3d7eb3e9e874a19a7ee3c2b8d0f9615b25d99182
Nacho <kulujun@gmail.com>
2023-07-14 17:56:53 +0800

    事件驱动-导入文件

---
+++ ToolKits/BikaResourcesToolKit.cs
@@ -0,0 +1,16 @@
+﻿using ShadowViewer.Interfaces;
+using System.Diagnostics;
+
+namespace ShadowViewer.ToolKits
+{
+    public class BikaResourcesToolKit: IResourcesToolKit
+    {
+        private readonly ResourceManager resourceManager = new ResourceManager();
+        private readonly string prefix = "ShadowViewer.Plugin.Bika/Resources/";
+        public BikaResourcesToolKit() { }
+        public string GetString(string key)
+        {
+            return resourceManager.MainResourceMap.GetValue(this.prefix + key.Replace(".", "/")).ValueAsString;
+        }
+    }
+}
