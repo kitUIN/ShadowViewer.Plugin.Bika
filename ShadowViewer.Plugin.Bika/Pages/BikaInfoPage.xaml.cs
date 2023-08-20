3ef040cc4e609c26123c80ddd963ea87a110a978
Nacho <kulujun@gmail.com>
2023-08-20 23:13:04 +0800

    修复一些bug

--- ShadowViewer.Plugin.Bika/Pages/BikaInfoPage.xaml.cs
+++ ShadowViewer.Plugin.Bika/Pages/BikaInfoPage.xaml.cs
@@ -1,4 +1,3 @@
-using System;
 using CustomExtensions.WinUI;
 using DryIoc;
 using Microsoft.UI.Xaml;
@@ -6,12 +5,12 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
 using PicaComic.Models;
-using Serilog;
 using ShadowViewer.Args;
 using ShadowViewer.Interfaces;
 using ShadowViewer.Plugin.Bika.Args;
 using ShadowViewer.Plugin.Bika.Enums;
 using ShadowViewer.Plugin.Bika.ViewModels;
+using ShadowViewer.Plugin.Local.Pages;
 
 namespace ShadowViewer.Plugin.Bika.Pages;
 
@@ -25,18 +24,17 @@ public sealed partial class BikaInfoPage : Page
         ViewModel = DiFactory.Services.Resolve<BikaInfoViewModel>();
         ViewModel.ScrollToCommentEvent += ViewModelOnScrollToCommentEvent;
     }
+
     private void GridV_ItemClick(object sender, ItemClickEventArgs e)
     {
-        if (e.ClickedItem is CategoryComic { IsLocked: false } comic)
-        {
-            Frame.Navigate(typeof(BikaInfoPage), comic.Id);
-        }
+        if (e.ClickedItem is CategoryComic { IsLocked: false } comic) Frame.Navigate(typeof(BikaInfoPage), comic.Id);
     }
-    private void ViewModelOnScrollToCommentEvent(object sender, ScrollToCommentEventArg e)
+
+    private void ViewModelOnScrollToCommentEvent(object? sender, ScrollToCommentEventArg e)
     {
-        var element =  CommentsItemsRepeater.GetOrCreateElement(e.Index);
+        var element = CommentsItemsRepeater.GetOrCreateElement(e.Index);
         CommentsItemsRepeater.UpdateLayout();
-        element.StartBringIntoView(new BringIntoViewOptions() { VerticalOffset = 0D, VerticalAlignmentRatio=0.0f });
+        element.StartBringIntoView(new BringIntoViewOptions() { VerticalOffset = 0D, VerticalAlignmentRatio = 0.0f });
     }
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -48,35 +46,29 @@ public sealed partial class BikaInfoPage : Page
 
     private void Author_OnClick(object sender, RoutedEventArgs e)
     {
-        if(sender is HyperlinkButton { Content: string tag })
+        if (sender is HyperlinkButton { Content: string tag })
             DiFactory.Services.Resolve<ICallableService>().NavigateTo(typeof(BikaCategoryPage),
                 new CategoryArg
                 {
-                    Category = tag , Mode = CategoryMode.Search
+                    Category = tag, Mode = CategoryMode.Search
                 }, true);
     }
 
     private void ChineseTeam_OnClick(object sender, RoutedEventArgs e)
     {
-        if(sender is HyperlinkButton { Content: string tag })
+        if (sender is HyperlinkButton { Content: string tag })
             DiFactory.Services.Resolve<ICallableService>().NavigateTo(typeof(BikaCategoryPage),
                 new CategoryArg
                 {
-                    Category = tag , Mode = CategoryMode.Search
+                    Category = tag, Mode = CategoryMode.Search
                 }, true);
     }
 
     private async void Segmented_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         if (RightGrid.SelectedIndex == 0)
-        {
             ViewModel.RefreshRecommendation();
-            
-        }
-        else if (ViewModel.Comments.Count == 0)
-        {
-            await ViewModel.LoadComments();
-        }
+        else if (ViewModel.Comments.Count == 0) await ViewModel.LoadComments();
     }
 
     private async void LikeComment_OnClick(object sender, RoutedEventArgs e)
@@ -89,25 +81,23 @@ public sealed partial class BikaInfoPage : Page
         if (sender is FrameworkElement { Tag: Comment comment }) await ViewModel.RefreshCommentChildren(comment);
     }
 
-    private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+    private void Page_SizeChanged(object sender, SizeChangedEventArgs? e)
     {
-        if (sender is Page page)
+        if (sender is not Page page) return;
+        if (MainGrid.RowDefinitions[1].ActualHeight == 0)
         {
-            if (MainGrid.RowDefinitions[1].ActualHeight == 0)
+            LeftScrollViewer.Height = page.ActualHeight - 20;
+            RightScrollViewer.Height = page.ActualHeight - 20;
+        }
+        else
+        {
+            LeftScrollViewer.Height = LeftGrid.ActualHeight;
+            if (RightGrid.SelectedIndex == 0)
             {
-                LeftScrollViewer.Height = page.ActualHeight - 20;
-                RightScrollViewer.Height = page.ActualHeight - 20;
+                // RightScrollViewer.Height;
             }
             else
             {
-                LeftScrollViewer.Height = LeftGrid.ActualHeight;
-                if (RightGrid.SelectedIndex == 0)
-                {
-                    // RightScrollViewer.Height;
-                }
-                else
-                {
-                }
             }
         }
     }
@@ -119,10 +109,8 @@ public sealed partial class BikaInfoPage : Page
 
     private async void ScrollViewer_OnViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
     {
-        if(sender is ScrollViewer viewer && viewer.VerticalOffset  + viewer.ActualHeight  + 2 >= viewer.ExtentHeight )
-        {
+        if (sender is ScrollViewer viewer && viewer.VerticalOffset + viewer.ActualHeight + 2 >= viewer.ExtentHeight)
             await ViewModel.LoadNextComments();
-        }
     }
 
     private void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
@@ -132,21 +120,19 @@ public sealed partial class BikaInfoPage : Page
 
     private void Tag_OnClick(object sender, RoutedEventArgs e)
     {
-        if(sender is Button { Content: string tag })
+        if (sender is Button { Content: string tag })
             DiFactory.Services.Resolve<ICallableService>().NavigateTo(typeof(BikaCategoryPage),
                 new CategoryArg
                 {
-                    Category = tag , Mode = CategoryMode.Search
+                    Category = tag, Mode = CategoryMode.Search
                 });
     }
 
     private void EpisodesButton_OnClick(object sender, RoutedEventArgs e)
     {
         if (sender is Button { Tag: Episode episode })
-        {
-            /*Frame.Navigate(typeof(PicPage), new PicViewArg(BikaPlugin.Meta.Id, new ComicArg
-                { ComicInfo = ViewModel.CurrentComic, CurrentEpisode = episode.Order, Episodes = ViewModel.Episodes }));*/
-        }
+            Frame.Navigate(typeof(PicPage), new PicViewArg(BikaPlugin.Meta.Id, new ComicArg
+                { ComicInfo = ViewModel.CurrentComic, CurrentEpisode = episode.Order, Episodes = ViewModel.Episodes }));
     }
 
     private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -156,10 +142,8 @@ public sealed partial class BikaInfoPage : Page
 
     private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
     {
-        if (sender is FrameworkElement { Tag: Creater creater } )
-        {
-            CreatorTip.CurrentUser = creater;
-            CreatorTip.Show();
-        } 
+        if (sender is not FrameworkElement { Tag: Creater creator }) return;
+        CreatorTip.CurrentUser = creator;
+        CreatorTip.Show();
     }
 }
